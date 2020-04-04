82ff696221f585ef36a7ef9c7d93c254235a7f44
Your Name <you@example.com>
2020-04-04 17:33:21 +0430

    remove dummy

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import  './styles/style.scss';
 import 'normalize.css/normalize.css';
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'Water bill' , amount:4500}))
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}))
-store.dispatch(addExpense({description: 'Rent' , amount:10905}))
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
 
 console.log(visibleExpenses);
 
